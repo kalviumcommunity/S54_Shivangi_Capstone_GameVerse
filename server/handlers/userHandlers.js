5187bcc94be8a5d89d001f34e0c77280127e5b62
shivangi-jain-08 <shivangi.jain@kalvium.community>
2024-06-18 13:22:51 +0530

    changed

--- server/handlers/userHandlers.js
+++ server/handlers/userHandlers.js
@@ -1,55 +1,55 @@
-const User = require('../models/userModel');
-const crypto = require('crypto')
-require('dotenv').config()
+// const User = require('../models/userModel');
+// const crypto = require('crypto')
+// require('dotenv').config()
 
-const createUser = async (req, res) => {
-    try {
-        const { userName, name, email, password } = req.body;
-        const hashedPassword = crypto.createHash('sha256').update(password).digest('hex');
-        const user = await User.find({ $or: [{ userName: userName }, { email: email }] });
-        if (user.length == 0) {
-            const newUser = new User({ userName: userName, name: name, email: email, password: hashedPassword });
-            await newUser.save();
-            res.status(201).json({ message: "User created successfully", data: newUser });
+// const createUser = async (req, res) => {
+//     try {
+//         const { userName, name, email, password } = req.body;
+//         const hashedPassword = crypto.createHash('sha256').update(password).digest('hex');
+//         const user = await User.find({ $or: [{ userName: userName }, { email: email }] });
+//         if (user.length == 0) {
+//             const newUser = new User({ userName: userName, name: name, email: email, password: hashedPassword });
+//             await newUser.save();
+//             res.status(201).json({ message: "User created successfully", data: newUser });
 
-        } else {
-            res.status(401).json({ message: "User already exists", data: user });
-        }
-    } catch (error) {
-        console.error("Error creating user:", error);
-        res.status(500).json({ message: "Error creating user" });
-    }
-};
+//         } else {
+//             res.status(401).json({ message: "User already exists", data: user });
+//         }
+//     } catch (error) {
+//         console.error("Error creating user:", error);
+//         res.status(500).json({ message: "Error creating user" });
+//     }
+// };
 
-const getAllUsers = async (req, res) => {
-    try {
-        const users = await User.find();
-        res.json(users);
-    } catch (error) {
-        console.error('Error fetching users:', error);
-        res.status(500).send('Internal server error');
-    }
-}
+// const getAllUsers = async (req, res) => {
+//     try {
+//         const users = await User.find();
+//         res.json(users);
+//     } catch (error) {
+//         console.error('Error fetching users:', error);
+//         res.status(500).send('Internal server error');
+//     }
+// }
 
-const loginUser = async (req, res) => {
-    try {
-        const { userName, password } = req.body;
-        const hashedPassword = crypto.createHash('sha256').update(password).digest('hex');
-        const user = await User.find({ userName: userName, password: hashedPassword });
-        if (user.length) {
-            res.status(200).json({ message: "Login successful", data: user });
-        } else {
-            res.status(401).json({ message: "Invalid username or password" });
-        }
-    } catch (error) {
-        console.error("Error logging in user:", error);
-        res.status(500).json({ message: "Error logging in user", error: error });
-    }
-};
+// const loginUser = async (req, res) => {
+//     try {
+//         const { userName, password } = req.body;
+//         const hashedPassword = crypto.createHash('sha256').update(password).digest('hex');
+//         const user = await User.find({ userName: userName, password: hashedPassword });
+//         if (user.length) {
+//             res.status(200).json({ message: "Login successful", data: user });
+//         } else {
+//             res.status(401).json({ message: "Invalid username or password" });
+//         }
+//     } catch (error) {
+//         console.error("Error logging in user:", error);
+//         res.status(500).json({ message: "Error logging in user", error: error });
+//     }
+// };
 
-const logoutUser = (req, res) => {
-    res.status(200).json({ message: 'Logout successful' });
-};
+// const logoutUser = (req, res) => {
+//     res.status(200).json({ message: 'Logout successful' });
+// };
 
 
-module.exports = { createUser, loginUser, logoutUser, getAllUsers };
+// module.exports = { createUser, loginUser, logoutUser, getAllUsers };
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const {createUser, loginUser, logoutUser, getAllUsers} = require('../handlers/userHandlers');
+// const express = require('express');
+// const {createUser, loginUser, logoutUser, getAllUsers} = require('../handlers/userHandlers');
 
 
-const userRoute = express.Router();
-userRoute.use(express.json())
+// const userRoute = express.Router();
+// userRoute.use(express.json())
 
-userRoute.get("/", getAllUsers);
-userRoute.post("/signup", createUser);
-userRoute.post("/login", loginUser);
-// userRoute.post("/logout", logoutUser)
+// userRoute.get("/", getAllUsers);
+// userRoute.post("/signup", createUser);
+// userRoute.post("/login", loginUser);
+// // userRoute.post("/logout", logoutUser)
 
-module.exports = userRoute;
+// module.exports = userRoute;
